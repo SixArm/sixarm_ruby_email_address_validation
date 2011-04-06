847487ed280f22cfdd4d6f46decff301700fa137
SixArm <sixarm@sixarm.com>
2011-04-06 03:05:02 -0700

    update for Ruby 1.9.2

--- lib/sixarm_ruby_email_address_validation.rb
+++ lib/sixarm_ruby_email_address_validation.rb
@@ -1,19 +1,19 @@
+# -*- coding: utf-8 -*-
 =begin rdoc
 
-= SixArm Ruby Gem: email address validation using RFC 822 pattern matching
+= SixArm » Ruby » Email address validation using RFC 822 pattern matching
 
 Author:: Joel Parker Henderson, joelparkerhenderson@gmail.com
-Copyright:: Copyright (c) 2006-2010 Joel Parker Henderson
-License:: CreativeCommons License, Non-commercial Share Alike
-License:: LGPL, GNU Lesser General Public License
+Copyright:: Copyright (c) 2006-2011 Joel Parker Henderson
+License:: See LICENSE.txt file           
 
 Email address regex, to validate an email address using RFC 822.
 
 This is a gem wrapper around http://tfletcher.com/lib/rfc822.rb
 
-Example:
+@example 
   if EMAIL_ADDRESS_PATTERN=~'foo@bar.com'
-    puts "found" 
+    puts "found"
   else
     puts "not found"
   end
@@ -22,7 +22,7 @@ Example:
 To find an email address in a string, do the pattern match
 then use the result, which is the match's string position:
 
-Example of match position:
+@example of match position:
   match_position = EMAIL_ADDRESS_PATTERN=~'foo@bar.com'
   => 0
 
@@ -32,20 +32,21 @@ Example of match position:
   match_position = EMAIL_ADDRESS_PATTERN=~'... hello world ...'
   => nil
 
+
 To do an exact pattern match, use the EMAIL_ADDRESS_EXACT_PATTERN.
 This matches the entire string from start to end; in other words,
 the entire string must be one email address.
 
-Example of exact pattern match:
-  if EMAIL_ADDRESS_EXACT_PATTERN=~'foo@bar.com' 
-    puts "found" 
+@example of exact pattern match:
+  if EMAIL_ADDRESS_EXACT_PATTERN=~'foo@bar.com'
+    puts "found"
   else
     puts "not found"
   end
   => found
 
-  if EMAIL_ADDRESS_EXACT_PATTERN=~'... foo@bar.com ...' 
-    puts "found" 
+  if EMAIL_ADDRESS_EXACT_PATTERN=~'... foo@bar.com ...'
+    puts "found"
   else
     puts "not found"
   end
@@ -63,20 +64,19 @@ Example to validate an email address:
 
 module EmailAddressValidation
 
-  EMAIL_ADDRESS_QTEXT = '[^\\x0d\\x22\\x5c\\x80-\\xff]'
-  EMAIL_ADDRESS_DTEXT = '[^\\x0d\\x5b-\\x5d\\x80-\\xff]'
-  EMAIL_ADDRESS_ATOM = '[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+'
-  EMAIL_ADDRESS_QUOTED_PAIR = '\\x5c[\\x00-\\x7f]'
-  EMAIL_ADDRESS_DOMAIN_LITERAL = "\\x5b(?:#{EMAIL_ADDRESS_DTEXT}|#{EMAIL_ADDRESS_QUOTED_PAIR})*\\x5d"
-  EMAIL_ADDRESS_QUOTED_STRING = "\\x22(?:#{EMAIL_ADDRESS_QTEXT}|#{EMAIL_ADDRESS_QUOTED_PAIR})*\\x22"
-  EMAIL_ADDRESS_DOMAIN_REF = EMAIL_ADDRESS_ATOM
-  EMAIL_ADDRESS_SUB_DOMAIN = "(?:#{EMAIL_ADDRESS_DOMAIN_REF}|#{EMAIL_ADDRESS_DOMAIN_LITERAL})"
-  EMAIL_ADDRESS_WORD = "(?:#{EMAIL_ADDRESS_ATOM}|#{EMAIL_ADDRESS_QUOTED_STRING})"
-  EMAIL_ADDRESS_DOMAIN = "#{EMAIL_ADDRESS_SUB_DOMAIN}(?:\\x2e#{EMAIL_ADDRESS_SUB_DOMAIN})*"
-  EMAIL_ADDRESS_LOCAL_PART = "#{EMAIL_ADDRESS_WORD}(?:\\x2e#{EMAIL_ADDRESS_WORD})*"
-  EMAIL_ADDRESS_SPEC = "#{EMAIL_ADDRESS_LOCAL_PART}\\x40#{EMAIL_ADDRESS_DOMAIN}"
-
-  EMAIL_ADDRESS_PATTERN = /#{EMAIL_ADDRESS_SPEC}/
-  EMAIL_ADDRESS_EXACT_PATTERN = /\A#{EMAIL_ADDRESS_SPEC}\z/
+  EMAIL_ADDRESS_QTEXT           = Regexp.new '[^\\x0d\\x22\\x5c\\x80-\\xff]', nil, 'n'
+  EMAIL_ADDRESS_DTEXT           = Regexp.new '[^\\x0d\\x5b-\\x5d\\x80-\\xff]', nil, 'n'
+  EMAIL_ADDRESS_ATOM            = Regexp.new '[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+', nil, 'n'
+  EMAIL_ADDRESS_QUOTED_PAIR     = Regexp.new '\\x5c[\\x00-\\x7f]', nil, 'n'
+  EMAIL_ADDRESS_DOMAIN_LITERAL  = Regexp.new "\\x5b(?:#{EMAIL_ADDRESS_DTEXT}|#{EMAIL_ADDRESS_QUOTED_PAIR})*\\x5d", nil, 'n'
+  EMAIL_ADDRESS_QUOTED_STRING   = Regexp.new "\\x22(?:#{EMAIL_ADDRESS_QTEXT}|#{EMAIL_ADDRESS_QUOTED_PAIR})*\\x22", nil, 'n'
+  EMAIL_ADDRESS_DOMAIN_REF      = EMAIL_ADDRESS_ATOM
+  EMAIL_ADDRESS_SUB_DOMAIN      = "(?:#{EMAIL_ADDRESS_DOMAIN_REF}|#{EMAIL_ADDRESS_DOMAIN_LITERAL})"
+  EMAIL_ADDRESS_WORD            = "(?:#{EMAIL_ADDRESS_ATOM}|#{EMAIL_ADDRESS_QUOTED_STRING})"
+  EMAIL_ADDRESS_DOMAIN          = "#{EMAIL_ADDRESS_SUB_DOMAIN}(?:\\x2e#{EMAIL_ADDRESS_SUB_DOMAIN})*"
+  EMAIL_ADDRESS_LOCAL_PART      = "#{EMAIL_ADDRESS_WORD}(?:\\x2e#{EMAIL_ADDRESS_WORD})*"
+  EMAIL_ADDRESS_SPEC            = "#{EMAIL_ADDRESS_LOCAL_PART}\\x40#{EMAIL_ADDRESS_DOMAIN}"
+  EMAIL_ADDRESS_PATTERN         = Regexp.new "#{EMAIL_ADDRESS_SPEC}", nil, 'n'
+  EMAIL_ADDRESS_EXACT_PATTERN   = Regexp.new "\\A#{EMAIL_ADDRESS_SPEC}\\z", nil, 'n'
 
 end
--- sixarm_ruby_email_address_validation.gemspec
+++ sixarm_ruby_email_address_validation.gemspec
@@ -1,18 +1,18 @@
 Gem::Specification.new do |s|
 
   s.name              = "sixarm_ruby_email_address_validation"
-  s.summary           = "SixArm Ruby Gem: Email address validation using RFC 822 pattern match regex regular expressions"
-  s.version           = "1.0.2"
+  s.summary           = "SixArm » Ruby » Email address validation using RFC 822 pattern match regex regular expressions"
+  s.version           = "1.2.0"
   s.author            = "SixArm"
   s.email             = "sixarm@sixarm.com"
   s.homepage          = "http://sixarm.com/"
-  s.signing_key       = '/home/sixarm/keys/certs/sixarm.com.rsa.private.key.and.certificate.pem'
-  s.cert_chain        = ['/home/sixarm/keys/certs/sixarm.pem']
+  s.signing_key       = '/home/sixarm/keys/certs/sixarm-rsa1024-x509-private.pem'
+  s.cert_chain        = ['/home/sixarm/keys/certs/sixarm-rsa1024-x509-public.pem']
 
   s.platform          = Gem::Platform::RUBY
   s.require_path      = 'lib'
   s.has_rdoc          = true
-  s.files             = ['README.rdoc','LICENSE.txt','lib/sixarm_ruby_email_address_validation.rb']
+  s.files             = ['README.rdoc','INSTALL.txt','LICENSE.txt','lib/sixarm_ruby_email_address_validation.rb']
   s.test_files        = ['test/sixarm_ruby_email_address_validation_test.rb']
 
 end
