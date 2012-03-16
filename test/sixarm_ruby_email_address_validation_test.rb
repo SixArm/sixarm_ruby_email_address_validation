e2fef6f5982899e9b258213e61a98bf13ca4b0aa
SixArm <sixarm@sixarm.com>
2012-03-16 09:55:20 -0700

    Tune up organization

--- test/sixarm_ruby_email_address_validation_test.rb
+++ test/sixarm_ruby_email_address_validation_test.rb
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 require 'minitest/autorun'
-require 'sixarm_ruby_email_address_validation'
 require 'simplecov'
 SimpleCov.start
+require 'sixarm_ruby_email_address_validation'
 
 class Testing < Test::Unit::TestCase
 
