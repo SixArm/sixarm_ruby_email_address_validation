d590ac5259d8653319187619b4cbdd3ef5c65e1b
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-07-07 11:36:37 -0600

    Refactor single-quote to double-quote

--- test/sixarm_ruby_email_address_validation_test.rb
+++ test/sixarm_ruby_email_address_validation_test.rb
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-require 'minitest/autorun'
-require 'simplecov'
+require "minitest/autorun"
+require "simplecov"
 SimpleCov.start
-require 'sixarm_ruby_email_address_validation'
+require "sixarm_ruby_email_address_validation"
 
 describe EmailAddressValidation do
 
